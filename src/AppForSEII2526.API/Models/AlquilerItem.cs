adacd119802d379233ccfd568c75d5ae0e9f3b07
gadealopez <127391352+gadealopez@users.noreply.github.com>
2025-10-03 12:37:00 +0200

    Crear clase AlquilerItem #17 (#23)

---
+++ src/AppForSEII2526.API/Models/AlquilerItem.cs
@@ -0,0 +1,51 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class AlquilerItem
+    {
+        public AlquilerItem()
+        {
+        }
+
+        //constructor que reciba coche y alquiler y los inicialice, ademas de inicializar las propiedades CocheId y AlquilerId con los ids de coche y alquiler respectivamente
+
+        public AlquilerItem(Coche coche, Alquiler alquiler)
+        {
+            Coche = coche;
+            CocheId = coche.Id;
+            Alquiler = alquiler;
+            AlquilerId = alquiler.Id;
+        }
+
+        //crear un constructor que reciba coche, alquiler y cantidad ya que llama al de arriba y ademas inicializa la cantidad
+
+        public AlquilerItem(Coche coche, Alquiler alquiler, double cantidad) : this(coche, alquiler)
+        {
+            Cantidad = cantidad;
+            
+        }
+
+        //constructor solo con los ids de coche y alquiler y la cantidad
+        public AlquilerItem(int cocheId, int alquilerId, double cantidad)
+        {
+            CocheId = cocheId;
+            AlquilerId = alquilerId;
+            Cantidad = cantidad;
+        }
+
+
+
+
+
+        public Coche Coche { get; set; }
+
+        public int CocheId { get; set; }
+
+
+        public Alquiler Alquiler { get; set; }
+
+        public int AlquilerId { get; set; }
+
+       
+        public double Cantidad { get; set; }
+    }
+}
