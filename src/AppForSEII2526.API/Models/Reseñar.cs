ba242d1d99c2e0f8cdd94a226f0c7d8b3199cff6
SANTIAGO FERNÁNDEZ LÓPEZ <Santiago.Fdez1@alu.uclm.es>
2025-10-04 18:24:24 +0200

    Añadida clase Reseñar con propiedades básicas y vínculo a ApplicationUser

---
+++ src/AppForSEII2526.API/Models/Reseñar.cs
@@ -0,0 +1,31 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    [Index(nameof(Reseñar), IsUnique = true)]
+    public class Reseñar
+    {
+        public Reseñar()
+        {
+        }
+
+        public Reseñar(int id, string usuario, string pais, string tipoConductor, DateTime creado, ApplicationUser applicationUser)
+        {
+            Id = id;
+            Usuario = usuario;
+            Pais = pais;
+            TipoConductor = tipoConductor;
+            Creado = creado;
+            ApplicationUser = applicationUser;
+        }
+
+        public int Id { get; set; }
+        public string Usuario { get; set; }
+        [Display(Name = "País")]
+        public string Pais { get; set; }
+        [Display(Name = "Tipo de conductor")]
+        public string TipoConductor { get; set; }
+        [Display(Name = "Fecha de creación")]
+        [DataType(DataType.DateTime)]
+        public DateTime Creado { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
