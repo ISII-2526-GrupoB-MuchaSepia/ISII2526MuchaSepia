503a9394597f48cf181383ac9b932546b7d48575
SANTIAGO FERNÁNDEZ LÓPEZ <Santiago.Fdez1@alu.uclm.es>
2025-10-04 18:28:58 +0200

    Creada clase ReseñarItem

---
+++ src/AppForSEII2526.API/Models/ReseñarItem.cs
@@ -0,0 +1,33 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    [PrimaryKey(nameof(ReseñarId), nameof(CocheId))]
+    public class ReseñarItem
+    {
+        public ReseñarItem()
+        {
+        }
+
+        public ReseñarItem(Coche coche, int rating, Reseñar reseñar, string descripcion)
+        {
+            Coche = coche;
+            CocheId = coche.Id;
+            Reseñar = reseñar;
+            ReseñarId = reseñar.Id;
+            Rating = rating;
+            Descripcion = descripcion;
+        }
+
+        public Coche Coche { get; set; }
+
+        public int CocheId { get; set; }
+
+        public Reseñar Reseñar { get; set; }
+
+        public int ReseñarId { get; set; }
+
+        [Range(1, 5, ErrorMessage = "El rating debe estar entre 1 y 5")]
+        public int Rating { get; set; }
+        [Display(Name = "Descripción")]
+        public string Descripcion { get; set; }
+    }
+}
