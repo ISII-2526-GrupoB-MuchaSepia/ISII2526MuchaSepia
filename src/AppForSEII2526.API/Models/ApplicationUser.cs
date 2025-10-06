4bd3bb67d379d1a2bd5ab17656b70d65b1f26e34
WildEnrique <Enrique.Vergara@alu.uclm.es>
2025-10-06 17:31:27 +0200

    clase applicationuser corregida

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -1,37 +1,8 @@
-﻿namespace AppForSEII2526.API.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
+﻿using Microsoft.AspNetCore.Identity;
 
+namespace AppForSEII2526.API.Models;
 
-        public ApplicationUser()
-        {
-        }
-
-        public ApplicationUser(string id, string nombre, string apellido, string nombreUsuario)
-        {
-            Id = id;
-            Nombre = nombre;
-            Apellido = apellido;
-            UserName = nombreUsuario;
-            Email = nombreUsuario;
-        }
-
-        [Display(Name = "Nombre")]
-        public string? Nombre
-        {
-            get;
-            set;
-        }
-
-        [Display(Name = "Apellido")]
-        public string? Apellido
-        {
-            get;
-            set;
-        }
-
-
-
-    }
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser{
 }
+
