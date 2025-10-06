d2b03493b3c3c1c6ba5fb45d659c673b957734b6
WildEnrique <Enrique.Vergara@alu.uclm.es>
2025-10-06 18:38:26 +0200

    clase compraritem creada

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++
@@ -1,7 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace AppForSEII2526.API.Models;
-
-// Add profile data for application users by adding properties to the ApplicationUser class
-public class ApplicationUser : IdentityUser {
-}
