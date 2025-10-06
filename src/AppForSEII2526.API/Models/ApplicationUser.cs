2709a1398ea79588589eb1c2ccc277b7179d8524
WildEnrique <Enrique.Vergara@alu.uclm.es>
2025-10-06 17:05:41 +0200

    clase compra creada

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -1,7 +1,37 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿namespace AppForSEII2526.API.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
 
-namespace AppForSEII2526.API.Models;
 
-// Add profile data for application users by adding properties to the ApplicationUser class
-public class ApplicationUser : IdentityUser {
+        public ApplicationUser()
+        {
+        }
+
+        public ApplicationUser(string id, string nombre, string apellido, string nombreUsuario)
+        {
+            Id = id;
+            Nombre = nombre;
+            Apellido = apellido;
+            UserName = nombreUsuario;
+            Email = nombreUsuario;
+        }
+
+        [Display(Name = "Nombre")]
+        public string? Nombre
+        {
+            get;
+            set;
+        }
+
+        [Display(Name = "Apellido")]
+        public string? Apellido
+        {
+            get;
+            set;
+        }
+
+
+
+    }
 }
---
+++ src/AppForSEII2526.API/Models/Comprar.cs
@@ -0,0 +1,52 @@
+﻿
+namespace AppForSEII2526.API.Models
+{
+    public class Comprar
+    {
+        public Comprar()
+        {
+        }
+
+        public Comprar(int id, string nombre, string apellido, string concesionarioEntrega,
+                      DateTime fechaCompra, decimal precioCompra, MetodoPagoTipos metodoPago, ApplicationUser applicationUser)
+        {
+            Id = id;
+            Nombre = nombre;
+            Apellido = apellido;
+            ConcesionarioEntrega = concesionarioEntrega;
+            FechaCompra = fechaCompra;
+            PrecioCompra = precioCompra;
+            MetodoPago = metodoPago;
+            ApplicationUser = applicationUser;
+        }
+
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        [Display(Name = "Concesionario de entrega")]
+        public string ConcesionarioEntrega { get; set; }
+
+        [Precision(10, 2)]
+        [Display(Name = "Precio de compra")]
+        public decimal PrecioCompra { get; set; }
+
+        [Display(Name = "Fecha de compra")]
+        public DateTime FechaCompra { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        [Display(Name = "Método de pago")]
+        public MetodoPagoTipos MetodoPago { get; set; }
+
+        public enum MetodoPagoTipos
+        {
+            TarjetaCredito,
+            PayPal,
+            Efectivo
+        }
+    }
+}
+
