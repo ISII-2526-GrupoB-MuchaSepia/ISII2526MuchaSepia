e7ddf286b90d9ff52fe924109fc1d983b2bcf1dc
gadealopez <127391352+gadealopez@users.noreply.github.com>
2025-10-06 17:28:37 +0200

    clase de usuario implementada correctamente #24 (#30)
    Clase usuario creada correctamente

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -4,4 +4,35 @@ namespace AppForSEII2526.API.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser {
+
+
+    public ApplicationUser()
+    {
+    }
+
+    public ApplicationUser(string id, string nombre, string apellido, string nombreUsuario)
+    {
+        Id = id;
+        Nombre = nombre;
+        Apellido = apellido;
+        UserName = nombreUsuario;
+        Email = nombreUsuario;
+    }
+
+    [Display(Name = "Nombre")]
+    public string? Nombre
+    {
+        get;
+        set;
+    }
+
+    [Display(Name = "Apellido")]
+    public string? Apellido
+    {
+        get;
+        set;
+    }
+
+
+
 }
