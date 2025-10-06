c90bcda9dce5d41e938dd1328688d51058db520b
gadealopez <127391352+gadealopez@users.noreply.github.com>
2025-10-06 19:16:19 +0200

    clase alquiler modificada correctamente. (#42)
    revisado

---
+++ src/AppForSEII2526.API/Models/Alquiler.cs
@@ -0,0 +1,57 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Alquiler
+    {
+        public Alquiler()
+        {
+        }
+        public Alquiler(string nombre, string apellido, string concesionarioEntrega, DateTime fechaAlquiler, PaymentMethodTypes metodoPago, DateTime inicioAlquiler, DateTime finAlquiler,  ApplicationUser applicationUser)
+        {
+            
+
+
+            InicioAlquiler = inicioAlquiler;
+            FinAlquiler = finAlquiler;
+            FechaAlquiler = fechaAlquiler;
+            ConcesionarioEntrega = concesionarioEntrega;
+            Nombre = nombre;
+            Apellido = apellido;
+           
+            MetodoPago = metodoPago;
+            ApplicationUser = applicationUser;
+        }
+
+        public int Id { get; set; }
+
+        public double Total { get; set; }
+
+        public DateTime FechaAlquiler { get; set; }
+
+        public DateTime InicioAlquiler { get; set; }
+        public DateTime FinAlquiler { get; set; }
+
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.MultilineText)]
+        [Display(Name = "Concesionario de entrega ")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor, ingresa tu dirección de entrega")]
+        public string ConcesionarioEntrega { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+        [Display(Name = "Metodos de pago")]
+        public PaymentMethodTypes MetodoPago { get; set; }
+
+       
+        public ApplicationUser ApplicationUser { get; set; }
+
+
+    }
+
+    public enum PaymentMethodTypes
+    {
+        CreditCard,
+        PayPal,
+        Cash
+    }
+}
