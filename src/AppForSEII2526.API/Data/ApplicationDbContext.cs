ed7a3d64091064956eef2a1aea1183f700d1d173
gadealopez <127391352+gadealopez@users.noreply.github.com>
2025-10-08 10:53:25 +0200

    clase ApplicationDbContext creada correctamente (#65)

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -2,7 +2,26 @@
 using Microsoft.EntityFrameworkCore;
 using AppForSEII2526.API.Models;
 
+
 namespace AppForSEII2526.API.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+
+        base.OnModelCreating(builder);
+
+        builder.Entity<AlquilerItem>().HasKey(pi => new { pi.CocheId, pi.AlquilerId });
+        builder.Entity<ComprarItem>().HasKey(pi => new { pi.CocheId, pi.ComprarId });
+        builder.Entity<ReseñarItem>().HasKey(pi => new { pi.CocheId, pi.ReseñarId });
+    }
+
+    public DbSet<Modelo> Modelos { get; set; }
+    public DbSet<Coche> Coches { get; set; }
+    public DbSet<Alquiler> Alquileres { get; set; }
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+    public DbSet<Comprar> Compras { get; set; }
+    public DbSet<Reseñar> Reseñas { get; set; }
 }
