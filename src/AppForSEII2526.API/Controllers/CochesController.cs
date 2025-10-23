e0ae54976453d8f8cae5916e3d7ca1a04c4370ae
gadealopez <Gadea.Lopez@alu.uclm.es>
2025-10-23 09:24:42 +0200

    Controller de coches

---
+++ src/AppForSEII2526.API/Controllers/CochesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CochesController : ControllerBase
+    {
+        private readonly ApplicationDbContext context; //permite consultar base de datos
+        private readonly ILogger<CochesController> _logger; //permite registrar logs
+
+        public CochesController(ApplicationDbContext context, ILogger<CochesController> logger)
+        {
+            this.context = context;
+            this._logger = logger;
+        }
+
+       
+
+
+
+        }
+}
