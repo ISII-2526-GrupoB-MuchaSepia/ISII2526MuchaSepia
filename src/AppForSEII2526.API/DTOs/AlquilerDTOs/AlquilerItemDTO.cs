65d30d3f460e2ce81b0e576f380bd1d5782d7d70
gadealopez <Gadea.Lopez@alu.uclm.es>
2025-10-24 20:17:38 +0200

    DTOs de alquiler creados correctamente

---
+++ src/AppForSEII2526.API/DTOs/AlquilerDTOs/AlquilerItemDTO.cs
@@ -0,0 +1,40 @@
+﻿namespace AppForSEII2526.API.DTOs.AlquilerDTOs
+{
+    public class AlquilerItemDTO
+    {
+        //representar cada coche dentro de un alquiler.
+        //El sistema muestra los coches seleccionados, indicando su modelo, fabricante y
+        // precio de alquiler y la cantidad de cada coche seleccionado
+
+        public AlquilerItemDTO(int cocheId,int cantidad, double precioAlquiler, string modelo, string fabricante ) { 
+            CocheId = cocheId; //El coche que es 
+            Cantidad = cantidad; //unidades alquiladas
+            PrecioAlquiler = precioAlquiler;
+            Modelo = modelo;
+            Fabricante = fabricante;
+        }
+        public int CocheId { get; set; }
+        public int Cantidad { get; set; }
+
+        public string Modelo { get; set; }
+
+        public string Fabricante { get; set; }
+
+        public double PrecioAlquiler { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is AlquilerItemDTO dTO &&
+                   CocheId == dTO.CocheId &&
+                   Cantidad == dTO.Cantidad &&
+                   PrecioAlquiler == dTO.PrecioAlquiler &&
+                   Modelo == dTO.Modelo &&
+                   Fabricante == dTO.Fabricante;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(CocheId, Cantidad,PrecioAlquiler,Modelo,Fabricante);
+        }
+    }
+}
