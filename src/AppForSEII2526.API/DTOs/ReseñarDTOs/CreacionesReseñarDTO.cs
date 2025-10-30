c386068e9f1efe9ac596935f14cb0159b15fbc2d
SANTIAGO FERNÁNDEZ LÓPEZ <Santiago.Fdez1@alu.uclm.es>
2025-10-30 23:13:44 +0100

    Creados los DTOs del caso de uso reseñar

---
+++ src/AppForSEII2526.API/DTOs/ReseñarDTOs/CreacionesReseñarDTO.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppForSEII2526.API.DTOs.ReseñarDTOs
+{
+    public class CreacionesReseñarDTO
+    {
+        public CreacionesReseñarDTO(
+            string usuario,
+            string tipoConductor,
+            string pais,
+            DateTime creado)
+        {
+            Usuario = usuario ?? throw new ArgumentNullException(nameof(usuario));
+            TipoConductor = tipoConductor ?? throw new ArgumentNullException(nameof(tipoConductor));
+            Pais = pais ?? throw new ArgumentNullException(nameof(pais));
+            Creado = creado;
+        }
+
+        public CreacionesReseñarDTO()
+        {
+        }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El usuario es obligatorio")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El usuario debe tener entre 3 y 50 caracteres")]
+        public string Usuario { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El tipo de conductor es obligatorio")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El tipo de conductor debe tener entre 3 y 50 caracteres")]
+        public string TipoConductor { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El país es obligatorio")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El país debe tener entre 3 y 50 caracteres")]
+        public string Pais { get; set; }
+
+        [Required]
+        [System.ComponentModel.DataAnnotations.DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        [Display(Name = "Fecha de creación")]
+        public DateTime Creado { get; set; }
+
+        protected bool CompareDate(DateTime date1, DateTime date2)
+        {
+            return (date1.Subtract(date2) < new TimeSpan(0, 1, 0));
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is CreacionesReseñarDTO dto &&
+                   Usuario == dto.Usuario &&
+                   TipoConductor == dto.TipoConductor &&
+                   Pais == dto.Pais &&
+                   CompareDate(Creado, dto.Creado);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Usuario, TipoConductor, Pais, Creado);
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/ReseñarDTOs/ReseñarItemDTO.cs
@@ -0,0 +1,39 @@
+﻿namespace AppForSEII2526.API.DTOs.ReseñarDTOs
+{
+    public class ReseñarItemDTO
+    {
+        public ReseñarItemDTO(int reseñarId, int cocheId, string cocheNombre, int rating, string descripcion = "")
+        {
+            ReseñarId = reseñarId;
+            CocheId = cocheId;
+            CocheNombre = cocheNombre;
+            Rating = rating;
+            Descripcion = descripcion;
+        }
+
+        public int ReseñarId { get; set; }
+
+        public int CocheId { get; set; }
+
+        public string CocheNombre { get; set; }
+
+        public int Rating { get; set; }
+
+        public string? Descripcion { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ReseñarItemDTO dto &&
+                   ReseñarId == dto.ReseñarId &&
+                   CocheId == dto.CocheId &&
+                   CocheNombre == dto.CocheNombre &&
+                   Rating == dto.Rating &&
+                   Descripcion == dto.Descripcion;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(ReseñarId, CocheId, CocheNombre, Rating, Descripcion);
+        }
+    }
+}
