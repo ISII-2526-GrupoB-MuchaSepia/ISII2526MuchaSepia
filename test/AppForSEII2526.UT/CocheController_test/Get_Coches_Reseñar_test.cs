ff50c2a0c42430b2bfb02f6858e6fedfd4b02752
SANTIAGO FERNÁNDEZ LÓPEZ <Santiago.Fdez1@alu.uclm.es>
2025-11-19 13:08:03 +0100

    Get Coches para reseñar creado

---
+++ test/AppForSEII2526.UT/CocheController_test/Get_Coches_Reseñar_test.cs
@@ -0,0 +1,88 @@
+﻿using AppForSEII2526.API.Controllers;
+using AppForSEII2526.API.DTOs.ReseñarDTOs;
+using AppForSEII2526.Shared.CocheDTOs;
+using AppForSEII2526.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AppForSEII2526.UT.CocheController_test
+{
+    public class Get_Coches_Reseñar_test : AppForSEII25264SqliteUT
+    {
+        public Get_Coches_Reseñar_test()
+        {
+            var modelos = new List<Modelo>
+            {
+                new Modelo("Mercedes Clase C"),
+                new Modelo("Volkswagen Golf"),
+                new Modelo("Toyota Corolla")
+            };
+
+            var coches = new List<Coche>
+            {
+                new Coche("Berlina", "Negro", "Sedán premium", "180 CV", "Diesel", "Mercedes", 25000m, 4, 2, 120, "17", modelos[0], Coche.TipoMantenimiento.Aceite),
+                new Coche("Compacto", "Azul", "Versátil y eficiente", "110 CV", "Gasolina", "Volkswagen", 20000m, 6, 3, 80, "16", modelos[1], Coche.TipoMantenimiento.Frenos),
+                new Coche("Sedán", "Blanco", "Cómodo para familias", "140 CV", "Híbrido", "Toyota", 23000m, 7, 2, 90, "17", modelos[2], Coche.TipoMantenimiento.Refrigeracion)
+            };
+
+            _context.AddRange(modelos);
+            _context.AddRange(coches);
+            _context.SaveChanges();
+        }
+
+        public static IEnumerable<object[]> TestCasesFor_GetCochesParaReseñar_OK()
+        {
+            var cocheDTOs = new List<CocheParaReseñarDTO>
+            {
+                new CocheParaReseñarDTO(1, "Berlina", "Negro", "Sedán premium", "Mercedes Clase C"),
+                new CocheParaReseñarDTO(2, "Compacto", "Azul", "Versátil y eficiente", "Volkswagen Golf"),
+                new CocheParaReseñarDTO(3, "Sedán", "Blanco", "Cómodo para familias", "Toyota Corolla")
+            };
+
+            var test1 = new List<CocheParaReseñarDTO> { cocheDTOs[0], cocheDTOs[1], cocheDTOs[2] };
+            var test2 = new List<CocheParaReseñarDTO> { cocheDTOs[1] };
+            var test3 = new List<CocheParaReseñarDTO> { cocheDTOs[2] };
+
+            var allTests = new List<object[]>
+            {
+                new object[] { null, null, test1 },
+                new object[] { "Compacto", null, test2 },
+                new object[] { null, "Blanco", test3 }
+            };
+
+            return allTests;
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCasesFor_GetCochesParaReseñar_OK))]
+        [Trait("Database", "WithoutFixture")]
+        [Trait("LevelTesting", "Unit Testing")]
+        public async Task GetCochesParaReseñar_OK_test(string? claseCoche, string? color, List<CocheParaReseñarDTO> expected)
+        {
+            // Arrange
+            var mock = new Moq.Mock<ILogger<CocheController>>();
+            var controller = new CocheController(_context, mock.Object);
+
+            // Act
+            var result = await controller.GetCochesParaReseñar(claseCoche, color);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var cochesActual = Assert.IsAssignableFrom<IList<CocheParaReseñarDTO>>(okResult.Value);
+
+            Assert.Equal(expected.Count, cochesActual.Count);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i].Id, cochesActual[i].Id);
+                Assert.Equal(expected[i].ClaseCoche, cochesActual[i].ClaseCoche);
+                Assert.Equal(expected[i].Color, cochesActual[i].Color);
+                Assert.Equal(expected[i].Descripcion, cochesActual[i].Descripcion);
+                Assert.Equal(expected[i].Modelo, cochesActual[i].Modelo);
+            }
+        }
+    }
+}
