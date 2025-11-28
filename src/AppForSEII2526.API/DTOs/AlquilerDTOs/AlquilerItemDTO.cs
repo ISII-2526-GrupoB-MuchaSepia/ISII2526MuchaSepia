a200d83c99f8095fa62e590141319152fbf8659b
gadealopez <Gadea.Lopez@alu.uclm.es>
2025-11-28 22:33:42 +0100

    cambios.

--- src/AppForSEII2526.API/DTOs/AlquilerDTOs/AlquilerItemDTO.cs
+++ src/AppForSEII2526.API/DTOs/AlquilerDTOs/AlquilerItemDTO.cs
@@ -12,7 +12,7 @@
             Cantidad = cantidad; //unidades alquiladas
             PrecioAlquiler = precioAlquiler;
             Modelo = modelo;
-            Fabricante = fabricante; //fabricante del coche
+            Fabricante = fabricante; //fabricante del    coche    
         }
 
         [Range(1, int.MaxValue)]
