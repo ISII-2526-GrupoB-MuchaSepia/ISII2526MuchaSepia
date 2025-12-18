ece89a6fa3f804b4cf5d9b4dfa3a833857a48bd6
Santifdezz1 <153225710+Santifdezz1@users.noreply.github.com>
2025-12-18 11:11:28 +0100

    Test examen subido (#243)
    Co-authored-by: Guillermo Fernandez <GuillermoTomas.Fernandez@alu.uclm.es>

--- test/AppForSEII2526.UIT/CU_Reseñar/CU_ReseñarCoches_UIT.cs
+++ test/AppForSEII2526.UIT/CU_Reseñar/CU_ReseñarCoches_UIT.cs
@@ -4,6 +4,7 @@ using Microsoft.VisualStudio.TestPlatform.Utilities;
 using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Threading;
 using Xunit;
 using Xunit.Abstractions;
@@ -12,20 +13,27 @@ namespace AppForSEII2526.UIT.CU_Reseñar
 {
     public class CU_ReseñarCoches_UIT : UC_UIT
     {
-        // DATOS DE PRUEBA 
+        // DATOS DE PRUEBA 1 EXAMEN
         private const string carId1 = "1";
         private const string model1 = "Mercedes Clase C";
         private const string clase1 = "Berlina";
         private const string color1 = "Negro";
         private const string desc1 = "Sedán premium de tamaño medio";
 
+        // DATOS DE PRUEBA 2 EXAMEN
+        private const string carId2 = "2";
+        private const string model2 = "Volkswagen Golf";
+        private const string clase2 = "Compacto Urbano";
+        private const string color2 = "Azul";
+        private const string desc2 = "Compacto alemán muy vendido";
+
         // USUARIO REAL
         private const string usuarioReal = "lucas.mdn";
         private const string pais = "España";
         private const string tipoConductor = "Titular";
 
-        // Datos válidos
-        private const string descripcionValida = "Reseña para Mercedes buena";
+        // Datos válidos 
+        private const string descripcionValida = "Reseña para Volkswagen buena examen"; //PARA EXAMEN
         private const int calificacionValida = 5;
 
         // --- PAGE OBJECTS ---
@@ -59,6 +67,47 @@ namespace AppForSEII2526.UIT.CU_Reseñar
 
             _selectCochesPO.SearchCars(filterClase, filterColor);
             Assert.True(_selectCochesPO.CheckListOfCars(expectedCars));
+        }
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void CU_Reseñar_FlujoCompleto_Examen()
+        {
+
+            InitialStepsForReviewCars();
+
+            var expectedReviewItems = new List <string[]> {
+                 new string[] {clase2, calificacionValida.ToString(), descripcionValida }
+               };
+            
+
+        // Act -> Selección
+        _selectCochesPO.SearchCars(clase1, "");
+            Thread.Sleep(1000);
+            _selectCochesPO.AddCarToReviewList(carId1);
+            Thread.Sleep(1000);
+            _selectCochesPO.SearchCars("", ""); //limpieza filtro
+            Thread.Sleep(1000);
+            _selectCochesPO.SearchCars("", color2);
+            Thread.Sleep(1000);
+            _selectCochesPO.AddCarToReviewList(carId2);
+            Thread.Sleep(1000);
+            _selectCochesPO.RemoveCarFromReviewList(carId1);
+            Thread.Sleep(1000);
+
+            _selectCochesPO.ClickWriteReviews();
+
+            // Act -> Rellenar formulario
+            _crearReseñaPO.RellenarDatosReseña(usuarioReal, pais, tipoConductor);
+            Thread.Sleep(1000);
+            _crearReseñaPO.RellenarDatosCoche(descripcionValida, calificacionValida, carId2);
+            Thread.Sleep(1000);
+
+            _crearReseñaPO.PulsarPublicarReseñas();
+
+            // Assert -> Verificar página de detalles
+            Assert.True(_detalleReseñaPO.CheckReviewDetail(usuarioReal, pais, tipoConductor, DateTime.Now));
+            Thread.Sleep(1000);
+            Assert.True(_detalleReseñaPO.CheckListOfReview(expectedReviewItems));
         }
 
         [Fact]
